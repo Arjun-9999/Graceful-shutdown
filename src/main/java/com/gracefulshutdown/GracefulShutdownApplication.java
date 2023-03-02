637de53020683c55c738133909ca182c890524f1
Anil Kuma Reddy <anilkumar1995708@gmail.com>
2023-03-02 13:41:41 +0530

    cccxc

--- src/main/java/com/gracefulshutdown/GracefulShutdownApplication.java
+++ src/main/java/com/gracefulshutdown/GracefulShutdownApplication.java
@@ -1,11 +1,8 @@
 package com.gracefulshutdown;
 
-import javax.annotation.PreDestroy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
@@ -13,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class GracefulShutdownApplication {
 
 	public static void main(String[] args) {
+		
+		System.out.println("**************************  ");
 		SpringApplication.run(GracefulShutdownApplication.class, args);
 		
 		
